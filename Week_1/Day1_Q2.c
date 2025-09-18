13c12cdeb30b919b085604dc2a16523c081350f6
uncoiledquest <neurallane752@gmail.com>
2025-09-18 10:40:36 +0530

    Q2 of Day1

---
+++ Week_1/Day1_Q2.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+int a,b;
+void add();
+void sub();
+void multiply();
+void divide();
+int main()
+{
+    printf("Enter first number: ");
+    scanf("%i",&a);
+    printf("Enter second number: ");
+    scanf("%i",&b);
+    add();
+    sub();
+    multiply();
+    divide();
+    
+
+
+}
+void add(){
+    int sum=a+b;
+    printf("Sum is: %i\n", sum);
+
+}
+void sub(){
+    int dif=a-b;
+    printf("Difference is: %i\n", dif);
+}
+
+void multiply(){
+    int mul=a*b;
+    printf("Multiplication is: %i\n", mul);
+}
+
+void divide(){
+    if(b==0){
+        printf("Division by zero is not allowed");
+        return;
+    }
+    float div=(float)a/b;
+    printf("Division is: %f\n", div);
+}
